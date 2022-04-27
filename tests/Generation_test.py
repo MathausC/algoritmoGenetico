8a87150c8418ef502b40b8649db6e118dc0520f9
MathausC <mathaus273@gmail.com>
2022-04-27 13:13:21 -0300

    Adiciona 'test_best_ones_was_maintained' teste

--- tests/Generation_test.py
+++ tests/Generation_test.py
@@ -19,3 +19,14 @@ class TestGeneration(unittest.TestCase):
         self.generation_object.troneio()
         self.generation = self.generation_object.get_generation()
         self.assertEqual(len(self.generation), self.size, 'A geração não tem tamanho correto após o troneio')
+
+    def test_best_ones_was_maintained(self):
+        bestones = self.generation_object.get_tres_melhores()
+        self.assertIn(bestones[0], self.generation, "Indivíduo 1 nunca esteve presente na geração")
+        self.assertIn(bestones[1], self.generation, "Indivíduo 2 nunca esteve presente na geração")
+        self.assertIn(bestones[2], self.generation, "Indivíduo 3 nunca esteve presente na geração")
+        self.generation_object.troneio()
+        self.generation = self.generation_object.get_generation()
+        self.assertIn(bestones[0], self.generation, "Indivíduo 1 não foi adicionado a nova geração")
+        self.assertIn(bestones[1], self.generation, "Indivíduo 2 não foi adicionado a nova geração")
+        self.assertIn(bestones[2], self.generation, "Indivíduo 3 não foi adicionado a nova geração")
