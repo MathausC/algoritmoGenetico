b6ee52f9b23b3f9c13be1c73bffe864bfbc796bd
MathausC <mathaus273@gmail.com>
2022-04-27 14:00:23 -0300

    Muda método torneio para algoritimoGenertico

--- src/GenerationsHistory.py
+++ src/GenerationsHistory.py
@@ -9,47 +9,18 @@ class Generation:
     def get_generation(self):
         return self.current_generation
 
-    def troneio(self):
+    def algoritmoGenetico(self, mutacao):
         new_generation = self.get_tres_melhores()
 
         while len(new_generation) < self.size:
-            copy_generation = self.current_generation.copy()
-         
-            first = 0
-            second = 0
-
-            while first == second:
-                first = random.randint(0, self.size-1)
-                second = random.randint(0, self.size-1)
-
-            parceiro1 = None
-
-            if copy_generation[first].peso >= copy_generation[second].peso:
-                parceiro1 = copy_generation.pop(first)
-            else:
-                parceiro1 = copy_generation.pop(second)
-
-            first = 0
-            second = 0
-
-            while first == second:
-                first = random.randint(0, self.size-2)
-                second = random.randint(0, self.size-2)
-
-            parceiro2 = None
-
-            if copy_generation[first].peso >= copy_generation[second].peso:
-                parceiro2 = copy_generation.pop(first)
-            else:
-                parceiro2 = copy_generation.pop(second)
-
+            parceiros = self.torneio()
 
             if self.size - len(new_generation) == 1:
-                descendente1 = PopulationGenerator.Individual(parceiro1.x1, parceiro2.x2, parceiro1.x3, parceiro2.x4, parceiro1.x5)
+                descendente1 = self.procriar(parceiros[0], parceiros[1], mutacao, True)
                 new_generation.append(descendente1)
             else:
-                descendente1 = PopulationGenerator.Individual(parceiro1.x1, parceiro2.x2, parceiro1.x3, parceiro2.x4, parceiro1.x5)
-                descendente2 = PopulationGenerator.Individual(parceiro2.x1, parceiro1.x2, parceiro2.x3, parceiro1.x4, parceiro2.x5)
+                descendente1 = self.procriar(parceiros[0], parceiros[1], mutacao, True)
+                descendente2 = self.procriar(parceiros[0], parceiros[1], mutacao, False)
                 new_generation.append(descendente1)
                 new_generation.append(descendente2)
 
