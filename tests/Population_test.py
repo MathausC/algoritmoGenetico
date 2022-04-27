95a2673613b913682023679314e87f1b8d4e3154
MathausC <mathaus273@gmail.com>
2022-04-27 10:43:53 -0300

    Adiciona 'test_element_type_generation' test

--- tests/Population_test.py
+++ tests/Population_test.py
@@ -7,5 +7,8 @@ class TestAIPopulation (unittest.TestCase) :
         self.size = 1000
         self.population = PopulationGenerator.generate_population(self.size)
 
-    def test_population_creation(self) :
+    def test_population_creation(self):
         self.assertEqual(len(self.population), self.size, 'The population has not 1000 individuals.')
+
+    def test_element_type_generation(self):
+        self.assertIsInstance(self.population[0], PopulationGenerator.Individual, "Os elementos da lista não são indivíduos.")
