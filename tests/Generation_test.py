b8ef9c0a9a4411c3b4c689738d39b09ea80215d4
MathausC <mathaus273@gmail.com>
2022-04-27 13:06:18 -0300

    Adiciona 'test_generation_size_after_turnment' teste'

--- tests/Generation_test.py
+++ tests/Generation_test.py
@@ -5,10 +5,17 @@ class TestGeneration(unittest.TestCase):
 
     def setUp(self):
         self.size = 1000
-        self.generation = GenerationsHistory.Generation(self.size).getGeneration()
+        self.generation_object = GenerationsHistory.Generation(self.size)
+        self.generation = self.generation_object.get_generation()
 
     def  test_generation_is_none(self):
         self.assertIsNotNone(self.generation, 'A geração é nula')
 
     def  test_generation_empty(self):
         self.assertEqual(len(self.generation), self.size, 'A geração não tem o mesmo tamanho que a anterior')
+
+    def test_generation_size_after_turnment(self):
+        self.assertEqual(len(self.generation), self.size, 'A geração não tem tamanho correto')
+        self.generation_object.troneio()
+        self.generation = self.generation_object.get_generation()
+        self.assertEqual(len(self.generation), self.size, 'A geração não tem tamanho correto após o troneio')
