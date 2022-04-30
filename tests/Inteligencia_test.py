342e6a2f27427c2876543e60a1ab084994193779
MathausC <mathaus273@gmail.com>
2022-04-30 15:08:46 -0300

    adiciona test de execução

--- tests/Inteligencia_test.py
+++ tests/Inteligencia_test.py
@@ -5,11 +5,15 @@ class TestingGeneticAlgoritm(unittest.TestCase):
     
     def setUp(self):
         self.size = 1000
-        self.faixa = 100
+        self.faixa = 1000
         self.mutacao = 10
         self.geracoes = 1000
+        self.repeticao = 1000
         self.inteligencia_object = IA.Inteligencia(self.size, self.faixa, self.mutacao, self.geracoes)
 
     def test_getbest_of_all_generations(self):
         self.assertIsNotNone(self.inteligencia_object, "Classe não foi instaciada.")
         self.assertIsNotNone(self.inteligencia_object.generation_object, "Classe geraçãoi não foi instaciada.")
+
+    def test_run_generarions(self):
+        self.inteligencia_object.run_generations(self.repeticao)
