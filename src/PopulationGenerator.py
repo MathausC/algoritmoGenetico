a52beb44da65d669cf5c0c548ace7dac9ba6dcbe
MathausC <mathaus273@gmail.com>
2022-04-30 15:08:11 -0300

    muda função de fit

--- src/PopulationGenerator.py
+++ src/PopulationGenerator.py
@@ -1,10 +1,11 @@
+from math import e
 import random
 
 class Individual:
 
     def __init__(self, x1, x2, x3, x4, x5) :
         self.xs = [x1, x2, x3, x4, x5]
-        self.peso = (self.xs[0]**5)+(self.xs[1]**4)+(self.xs[2]**3)+(self.xs[3]**2)+(self.xs[4]**1)
+        self.peso = ((sum(self.xs))*e**(-(sum(self.xs))/20))
 
     def imprime_peso_e_xs(self):
         print(self.peso)
